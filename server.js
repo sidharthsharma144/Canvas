e6fd436931c1d3cad5e8a75dbfb4a29452d91e73
AnshDabra27 <kambojansh2004@gmail.com>
2024-11-27 15:53:02 +0530

    databse connected

--- server.js
+++ server.js
@@ -1,22 +1,38 @@
 const express = require('express')
+const mongoose = require('mongoose') // Keep this import only
 const app = express()
 const dotenv = require('dotenv')
 const cors = require('cors')
-// hello commit succesful
 
 dotenv.config()
 
-if(process.env.NODE_ENV === 'local'){
+if (process.env.NODE_ENV === 'local') {
     app.use(cors({
         origin: 'http://localhost:3000',
         credentials: true
     }))
-}else{
+} else {
     app.use(cors({
-        credentials:true
+        credentials: true
     }))
 }
 
-const PORT = process.env.PORT
+const dbConnect = async () => {
+    try {
+        if (process.env.NODE_ENV === 'local') {
+            await mongoose.connect(process.env.LOCAL_DB_URI)
+            console.log('Local database is connected...')
+        } else {
+            await mongoose.connect(process.env.MONGODB_URI)
+            console.log('Production database is connected...')
+        }
+    } catch {
+        console.log('Database Connection Failed!')
+    }
+}
 
-app.listen(PORT,()=>console.log(`Server is running on port ${PORT}..`))
+dbConnect()
+
+const PORT = process.env.PORT || 3000 // Default to port 3000 if PORT is not defined
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}..`))
